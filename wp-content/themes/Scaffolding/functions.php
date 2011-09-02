65a308d08e65111ee20666af79cb8c18b16aaa6e
joshuwar <josh.u.war@gmail.com>
2011-09-02 19:08:36 +0100

    adding wp files

---
+++ wp-content/themes/Scaffolding/functions.php
@@ -0,0 +1,40 @@
+<?php 
+
+// Custom header constants
+define( 'HEADER_IMAGE_WIDTH', apply_filters( 'scaffolding_header_image_width', 220 ) );
+define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'scaffolding_header_image_height', 220 ) );
+define( 'NO_HEADER_TEXT', true );
+
+// gets included in the site header
+function header_style() {
+    ?><style type="text/css">
+        #header {
+            background: url(<?php header_image(); ?>);
+        }
+    </style><?php
+}
+// gets included in the admin header
+function admin_header_style() {
+    ?><style type="text/css">
+        #headimg {
+            width: <?php echo HEADER_IMAGE_WIDTH; ?>px;
+            height: <?php echo HEADER_IMAGE_HEIGHT; ?>px;
+            background: no-repeat;
+        }
+    </style><?php
+}
+
+// Allow for a custom header
+add_custom_image_header('header_style', 'admin_header_style');
+
+// Allow for a custom background
+add_custom_background();
+
+
+// Register Main and Footer Menu
+if ( function_exists( 'register_nav_menu' ) ) {
+	register_nav_menu( 'main_menu', 'Main Menu' );
+}
+
+
+?>
---
+++ wp-content/themes/ofjoshuabradley/js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	// Detect webkit browsers to alter letterspacing in CSS
+	
+	if ($.browser.webkit) {
+		$('body').addClass('webkit');
+	}
+});
