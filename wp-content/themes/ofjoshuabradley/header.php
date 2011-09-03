34305fb05971b6189b083846ff10e462f9dadb13
joshuwar <josh.u.war@gmail.com>
2011-09-03 18:14:14 +0100

    making top link work

--- wp-content/themes/ofjoshuabradley/header.php
+++ wp-content/themes/ofjoshuabradley/header.php
@@ -54,6 +54,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<a name="top" id="top"></a>
 	<div><a class="trigger" href="javascript:func()">Grid</a></div>
 	<div id="wrap" class="png_bg">
 		<div class="jbasewrap">
